e130cfe36b4c8cbfa4efb6170f787efd33ced012
Dominik Welland <hallo@doik.de>
2014-04-01 23:30:31 +0200

    adopt to partial-loading

--- README.md
+++ README.md
@@ -5,13 +5,11 @@ github-codereview
 
 This will add ``next commit``, ``previous commit``, ``back to pull request`` links to the site if you're reviewing a pull request. 
 
-Basically, if you visit the overview of the pull request, it will add an array of all commit hashes to all links that point to single commits. If you follow one of the links, it will obtain the next and previous commit hashes from this array. It's not very elegant, but it does the job ;)   
+Additionally, thanks to @witsch you can access these links via `n`, `p` and `b` access keys.
 
+Basically, if you visit the overview of the pull request, it will add an array of all commit hashes to all links that point to single commits. If you follow one of the links, it will obtain the next and previous commit hashes from this array. It's not very elegant, but it does the job ;)
 
-Known current issues
-=================
-1. does only work on the shorthash links in the pullrequest
-
+Since the last update, this also works for the recently introduced partial-loading approach.
 
 How to install
 =================
--- github.com.js
+++ github.com.js
@@ -1,27 +1,55 @@
+/*
 
-function getUrlVars() {
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
+github.com.js modifies the DOM for more
+convenience while reviewing pull requests.
+
+require dot.js
+
+https://github.com/Doik/github-codereview
+
+*/
+
+
+(
+
+function( $ ){
+
+    var hashes = [];
+    var pullrequest_url = null;
+    var current_commit = null;
+
+    function getUrlVars() {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
     }
-    return vars;
-}
 
+    function insertAjaxButtons(event){
+        if(window.location.pathname.match('/commit/')){
+            insertButtons();
+        }
+    }
 
-$(document).ready(function(){
-    if(location.href.match('/commit/') && location.search.match('hashes=')){
+    if(window.location.pathname.match('/commit/') && window.location.search.match('hashes=')){
         insertButtons();
     }
 
-    // var timetag = $('body.page-commit-show time.js-relative-date');
-    // var time = timetag.attr('title');
+    if(window.location.pathname.match('/pull/[0-9]+$')){
 
-    if(location.href.match('/pull/[0-9]+')){
+        pullrequest_url = window.location.pathname;
+
+        $('[data-pjax-container]').bind('DOMSubtreeModified', function(event){
+            if(window.location.href.match('/commit/')){
+                insertButtons();
+            }
+        });
+
         var links = [];
-        var hashes = [];
         $.each($('.timeline-commits tr'), function(idx, el){
             el = $(el);
             if(el.is(':visible')){
@@ -33,7 +61,7 @@ $(document).ready(function(){
         });
         var hashstr = '?hashes=' + hashes.join(',');
         //strip https://github.com
-        hashstr += '&pullrequest=' + location.href.substr(18);
+        hashstr += '&pullrequest=' + encodeURIComponent(pullrequest_url);
         $.each($('.timeline-commits tr .commit-message code a'), function(idx, el){
             el.href = el.href + hashstr;
             $(el).queue('click', insertButtons);
@@ -45,34 +73,50 @@ $(document).ready(function(){
 
     }
 
-function insertButtons(){
-        var vars = getUrlVars();
-        hashes = vars['hashes'].split(',');
-        var current_commit = $('.full-commit .commit-meta.clearfix .sha-block span.sha').text().substr(0, hashes[0].length);
-        var idx = hashes.indexOf(current_commit);
-        var prev = '';
-        var next = '';
-        var info = '<p class="bubble"><strong>commit ' + (idx + 1) + '/' + hashes.length + '</strong><p>';
-        if(idx > 0){
-            prev = '<a href="../commit/' + hashes[idx-1] + location.search + '" accesskey="p" class="minibutton" style="float: left;">&lt; go to previous commit</a>';
+    function insertButtons(){
+        var up_url;
+        if(hashes.length == 0){
+            var vars = getUrlVars();
+            hashes = vars['hashes'].split(',');
+            up_url = vars['pullrequest'];
         }
-        if(idx < hashes.length - 1){
-            next = '<a href="../commit/' + hashes[idx+1] + location.search + '" accesskey="n" class="minibutton" style="float: right;">go to next commit &gt;</a>';
+        else {
+            up_url = pullrequest_url;
         }
-        var up = '<a href="' + vars['pullrequest'] + '" accesskey="b" class="minibutton" style="text-align: center; display:block; width: 10em; margin: 0 auto;">back to pullrequest</a>';
-        var topmarkup = '<div style="margin: 1em 0; text-align:center;">' + info + prev + next + up +  '<p style="margin: 0; clear:both">&nbsp;</p></div>';
-        var bottommarkup = '<div style="margin: 1em 0; text-align:center;">' + prev + next + up +  '<p style="margin: 0; clear:both">&nbsp;</p></div>';
+        if(current_commit == window.location.pathname)
+            return;
+
         var bottom_el = $('#all_commit_comments');
         var top_el = $('#files');
-        bottom_el.html(bottommarkup + bottom_el.html());
+
+        if(!bottom_el.length || !top_el.length){
+            return
+        }
+
+        current_commit = window.location.pathname;
+        var current_commit_hash = window.location.pathname.split('/').reverse()[0].substr(0, hashes[0].length);
+        var idx = hashes.indexOf(current_commit_hash);
+        var prev = '';
+        var next = '';
+        var info = '<p id="dom_modified_by_dotjs_helper" class="bubble"><strong>commit ' + (idx + 1) + '/' + hashes.length + '</strong><p>';
+        if(idx > 0){
+            prev = '<a href="../commit/' + hashes[idx-1] + window.location.search + '" data-pjax="true" accesskey="p" class="minibutton" style="float: left;">&lt; go to previous commit</a>';
+        }
+        if(idx < hashes.length - 1){
+            next = '<a href="../commit/' + hashes[idx+1] + window.location.search + '" data-pjax="true" accesskey="n" class="minibutton" style="float: right;">go to next commit &gt;</a>';
+        }
+        var up = '<a href="' + decodeURIComponent(up_url) + '" accesskey="b" data-pjax="true" class="minibutton" style="text-align: center; display:block; width: 10em; margin: 0 auto;">back to pullrequest</a>';
+        var topmarkup = '<div style="margin: 1em 0; text-align:center;">' + info + prev + next + up +  '<p style="margin: 0; clear:both">&nbsp;</p></div>';
+        var bottommarkup = '<div style="margin: 1em 0; text-align:center;">' + prev + next + up +  '<p style="margin: 0; clear:both">&nbsp;</p></div>';
         top_el.html(topmarkup + top_el.html());
+        bottom_el.html(bottommarkup + bottom_el.html());
 
         $.each($('form'), function(idx, el){
             el = $(el);
             if(el.attr('action') && el.attr('action').match('commit_comment')){
-                el.attr('action', el.attr('action') + location.search);
+                el.attr('action', el.attr('action') + window.location.search);
             }
         });
-}
+    }
 
-});
+}(jQuery));
